a38988a32242a54c931a770197421fea246c9f89
Daan Meijer <daan@studioseptember.nl>
2018-01-09 15:22:27 +0100

    colormap geimplenteerd, run-sim aangezet

---
+++ run-sim.sh
@@ -0,0 +1,6 @@
+xrandr --output HDMI-0 --scale 0.75x1
+
+./bin/SARndbox -wo 15 -uhm -fpv -us -uhs -vruiVerbose -cp /tmp/control-socket -socket &
+sleep 5
+google-chrome-unstable --app-id=cgigkgkdfoolaeobhmpmcnkobgnmlkio &
+cd ~/src/node-sandbox; ~/Downloads/node-v8.9.4-linux-x64/bin/node index.js &
